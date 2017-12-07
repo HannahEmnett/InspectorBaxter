f93271bbb206b6045a63c4ef5d21d1f989b423a6
Srikanth Kilaru <srikilaru@gmail.com>
2017-12-06 20:23:04 -0600

    s

--- src/pcl_transform.py
+++ src/pcl_transform.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-from math import pi, sin, cos, dot
-from inspector.msgs import PclData
+from math import pi, sin, cos
+from inspector.msg import PclData
 
 def trans_points(data):
     pub = rospy.Publisher("inspector/pclData2", PclData, queue_size = 10)
@@ -11,7 +11,7 @@ def trans_points(data):
     kin_loc=np.array([[2],[-0.4],[0],[1]])
     #bot_loc=np.array([[0],[0.05],[1],[1]])
     out=PclData()
-    for i in range(1,len(xpos)):
+    for i in range(1,len()):
         bot_loc=[[float(data.centroid[i].x)],[float(data.centroid[i].y)],[float(data.centroid[i].z)]]
         kinect_to_bottle=rotx.dot(roty.dot(bot_loc))
         baxter_to_bottle=kinect_to_bottle+kin_loc
@@ -21,12 +21,9 @@ def trans_points(data):
         out.centroid[i].x=float(baxter_to_bottle[0])
         out.centroid[i].y=float(baxter_to_bottle[1])
         out.centroid[i].z=float(baxter_to_bottle[2])
-    pub.publish(out)
-    rospy.spin()
-
+        pub.publish(out)
 
 if __name__ == '__main__':
     rospy.init_node("pcl_transform")
-    pub = rospy.Publisher("inspector/pclData2", PclData, queue_size = 10)
-    rospy.Subscriber("inspector/pclData",PclData, trans_points
+    rospy.Subscriber("pclData",PclData, trans_points)
     rospy.spin()
