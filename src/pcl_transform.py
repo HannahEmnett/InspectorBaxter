46686ceceabe22f603508bbf108e485e01dc399a
HannahEmnett <hannahemnett@gmail.com>
2017-12-08 14:40:18 -0600

    updated for comments

--- src/pcl_transform.py
+++ src/pcl_transform.py
@@ -7,17 +7,20 @@ from geometry_msgs.msg import Point
 
 def trans_points(data):
     pub = rospy.Publisher("inspector/pclData2", PclData, queue_size = 10)
+    #initialziations, rotation matrices, and kinect location
+    out=PclData()
+    cent=Point()
     rotx=np.array([[1,0, 0 ,0],[0, cos(pi/2), -sin(-pi/2) ,0],[0 ,sin(-pi/2) ,cos(pi/2) ,0 ],[0 ,0 ,0 ,1]])
     roty=np.array([[cos(pi/2) ,0, sin(-pi/2), 0],[0 ,1 ,0 ,0],[-sin(-pi/2) ,0 ,cos(pi/2) ,0],[0, 0, 0, 1]])
     kin_loc=np.array([[1.565],[-0.335],[-0.12],[1]])
-    #kin_loc=np.array([[1.524],[-0.332],[-0.115],[1]])
-    #bot_loc=np.array([[0],[0.05],[1],[1]])
-    out=PclData()
-    cent=Point()
+
+    #do the transformation
     exact=data.centroid
     bot_loc=np.array([[float(exact.x)],[float(exact.y)],[float(exact.z)],[1]])
     kinect_to_bottle=rotx.dot(roty.dot(bot_loc))
     baxter_to_bottle=kinect_to_bottle+kin_loc
+
+    #store the transformed information into a new message
     cent.x=float(baxter_to_bottle[0])
     cent.y=float(baxter_to_bottle[1])
     cent.z=float(baxter_to_bottle[2])
@@ -25,9 +28,12 @@ def trans_points(data):
     out.height=data.height
     out.width=data.width
     out.ratio=data.ratio
+
+    #publish the message
     pub.publish(out)
 
 if __name__ == '__main__':
+    #initializations 
     rospy.init_node("pcl_transform")
     rospy.Subscriber("/pclData",PclData, trans_points)
     rospy.spin()
